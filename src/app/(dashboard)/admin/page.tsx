0cf1d6df6ecbbf190227f3ba122e6c6fb4e3b0ee
Tomoriu Akarin <lequocanh10000@github.com>
2025-10-14 17:20:23 +0700

    feat: add radial charts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,17 +1,29 @@
+import CountChart from "@/components/count-chart"
 import UserCard from "@/components/user-card"
 
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/* LEFT */}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
                 {/* USER CARD */}
                 <div className="flex gap-4 justify-between flex-wrap">
-                    <UserCard type="student"/>
-                    <UserCard type="teacher"/>
-                    <UserCard type="parent"/>
-                    <UserCard type="staff"/>
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
                 </div>
+                {/* CHART */}
+                <div className="flex gap-4 flex gap-4 flex-col lg:flex-row">
+                    {/* COUNT CHART */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
+                    </div>
+                    {/* ATTENDANCE CHART */}
+                    <div className="w-full lg:w-2/3 h-[450px]"></div>
+                </div>
+                {/* BOT */}
+                <div className=""></div>
             </div>
             {/* RIGHT */}
             <div className="w-full lg:w-1/3">r</div>
