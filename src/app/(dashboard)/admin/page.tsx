ff323e83c39d94726df73189264520706f84295f
Tomoriu Akarin <lequocanh10000@github.com>
2025-10-15 16:33:09 +0700

    feat: complete admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,8 @@
+import Annoucement from "@/components/annoucement"
+import AttendaanceChart from "@/components/attendance-chart"
 import CountChart from "@/components/count-chart"
+import EventCalendar from "@/components/event-calendar"
+import FinanceChart from "@/components/finance-chart"
 import UserCard from "@/components/user-card"
 
 const AdminPage = () => {
@@ -20,13 +24,20 @@ const AdminPage = () => {
                         <CountChart />
                     </div>
                     {/* ATTENDANCE CHART */}
-                    <div className="w-full lg:w-2/3 h-[450px]"></div>
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendaanceChart />
+                    </div>
                 </div>
                 {/* BOT */}
-                <div className=""></div>
+                <div className="w-full h-[500px]">
+                    <FinanceChart />
+                </div>
             </div>
             {/* RIGHT */}
-            <div className="w-full lg:w-1/3">r</div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalendar/>
+                <Annoucement/>
+            </div>
         </div>
     )
 }
