17793adc31539e1119d013cc1cd7e02be8d9b872
Tomoriu Akarin <lequocanh10000@github.com>
2025-10-21 15:04:15 +0700

    feat: fix teacher list page

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -99,9 +99,11 @@ const TeacherList = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
-                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                            <Image src="/plus.png" alt="" width={14} height={14} />
-                        </button>
+                        {role === 'admin' && (
+                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                                <Image src="/plus.png" alt="" width={14} height={14} />
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
